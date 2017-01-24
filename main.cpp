0ef8855a292d41cd48ee403cfc5a6e1ed7a938e0
Tim Viola <psalm842@gmail.com>
2017-01-23 22:17:10 -0500

    Interrupt Strategy for PWM Input
    The beginnings of an interrupt strategy for PWM input. Also queues may be handy? Or timers also?

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include "mbed.h"
 
-DigitalOut led1(LED1);
+PwmOut pwmOut1(PTD0);
+InterruptIn sw(PTC4);
+uint32_t riseCounter = 0;
+uint32_t fallCounter = 0;
 
-// main() runs in its own thread in the OS
-// (note the calls to Thread::wait below for delays)
-int main() {
-    while (true) {
-        led1 = !led1;
-        Thread::wait(350);
-    }
+void rise_handler(void) {
+    riseCounter++; 
 }
 
+void fall_handler(void) {
+    fallCounter++; 
+}
+
+int main() {
+	pwmOut1.period(0.0005f);
+	pwmOut1.write(0.5f);
+    sw.rise(rise_handler);
+    sw.fall(fall_handler);
+}
