3fe5d1c0f936895f4b9657985946bd4d09c32870
Tim Viola <psalm842@gmail.com>
2017-01-23 22:33:49 -0500

    Added Frequency Calculator
    Calculates the frequency by counting the number of interrupts that happen in a timespan between an RtosTimer function's calls (currently calls are 10ms apart)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ PwmOut pwmOut1(PTD0);
 InterruptIn sw(PTC4);
 uint32_t riseCounter = 0;
 uint32_t fallCounter = 0;
+const uint16_t pwmFreqCalculationPeriod = 10; //ms
 
 void rise_handler(void) {
     riseCounter++; 
@@ -13,9 +14,21 @@ void fall_handler(void) {
     fallCounter++; 
 }
 
+void periodCalculator(void) {
+	float frequency = (float)riseCounter/float(pwmFreqCalculationPeriod)*1000;
+	//TODO: Calculate pulseWidth
+}
+
+
 int main() {
+	RtosTimer periodTimer(periodCalculator, osTimerPeriodic);
+    
+    periodTimer.start(pwmFreqCalculationPeriod);
+    
 	pwmOut1.period(0.0005f);
 	pwmOut1.write(0.5f);
     sw.rise(rise_handler);
     sw.fall(fall_handler);
+
+    Thread::wait(osWaitForever);
 }
