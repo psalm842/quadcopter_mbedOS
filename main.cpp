7d9e8a60d8e445a85261139d585af7c84e843c7b
Tim Viola <psalm842@gmail.com>
2017-03-06 22:26:56 -0500

    FTM Input Capture working
    at long last, it captures input from PTA2 on PTD0

--- main.cpp
+++ main.cpp
@@ -12,8 +12,7 @@
   
 #define FTM_READ_INPUT_CAPTURE  FTM3->CONTROLS[kFTM_Chnl_0].CnV  
 
-//PwmOut pwmOut1(PTD0);
-uint32_t pwmFreqCalculationPeriod = 2000; // ms
+PwmOut pwmOut1(PTA2);
 
 void calculatePeriod(void) {
 	//float frequency = (float)FTM_READ_INPUT_CAPTURE/float(pwmFreqCalculationPeriod)*1000;
@@ -23,9 +22,7 @@ void calculatePeriod(void) {
 
 
 int main() {
-	pin_function(PTA2, kPORT_MuxAlt4);
-    pin_mode(PTA2,PullNone);
-    pin_function(PTD0, kPORT_MuxAlt4);
+    pin_function(PTD0, kPORT_MuxAlt4); // FTM3 Ch0
     pin_mode(PTD0, PullNone);
 
 
@@ -44,53 +41,21 @@ int main() {
 	ftm_config_t ftmInfo;    
     FTM_GetDefaultConfig(&ftmInfo); 
     ftmInfo.prescale = (ftm_clock_prescale_t)clkdiv; 
-    /* Initialize FTM module */  
-    FTM_Init(FTM0, &ftmInfo);
+    // Initialize FTM module  
     FTM_Init(FTM3, &ftmInfo);
     
-    /* Print a note to terminal */  
+    // Print a note to terminal 
     printf("\r\nFTM input capture example\r\n");  
-    //printf("Port A, PCR2 Register Value: %x",PORTA->PCR[2]);
-
-    ftm_chnl_pwm_signal_param_t config = {
-        .chnlNumber = kFTM_Chnl_7,
-        .level = kFTM_HighTrue,
-        .dutyCyclePercent = 20,
-        .firstEdgeDelayPercent = 0
-    };
-
-
-    FTM_SetupPwm(FTM0, &config, 1, kFTM_EdgeAlignedPwm, 50, 1);
-    FTM_StartTimer(FTM0, kFTM_SystemClock);
-
 
     FTM_SetupInputCapture(FTM3, kFTM_Chnl_0, kFTM_RisingEdge, 0);
-    //FTM0->MOD = 0xFFFF;
+    FTM3->MOD = 0xFFFF; // Modulo register must not be 0 for counter to work, see documentation
     FTM_StartTimer(FTM3, kFTM_SystemClock);
 
+    pwmOut1.period(0.010f); // 10ms
+    pwmOut1.write(0.50f);
+    
     // creates a queue to hold 1 events
     EventQueue queue(1*EVENTS_EVENT_SIZE);
-    queue.call_every(pwmFreqCalculationPeriod,calculatePeriod);
+    queue.call_every(20,calculatePeriod); //20ms 
     queue.dispatch();
 }
-
-
-/*!
- * @brief Enables capturing an input signal on the channel using the function parameters.
- *
- * When the edge specified in the captureMode argument occurs on the channel, the FTM counter is
- * captured into the CnV register. The user has to read the CnV register separately to get this
- * value. The filter function is disabled if the filterVal argument passed in is 0. The filter
- * function is available only for channels 0, 1, 2, 3.
- *
- * @param base        FTM peripheral base address
- * @param chnlNumber  The channel number
- * @param captureMode Specifies which edge to capture
- * @param filterValue Filter value, specify 0 to disable filter. Available only for channels 0-3.
- 
-void FTM_SetupInputCapture(FTM_Type *base,
-                           ftm_chnl_t chnlNumber,
-                           ftm_input_capture_edge_t captureMode,
-                           uint32_t filterValue);
-
-*/
