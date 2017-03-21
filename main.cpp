bab30a8962d6209f90fa46e1b837bd2c47069ff5
Tim Viola <psalm842@gmail.com>
2017-03-20 23:26:10 -0400

    Basic Motor Test + Comments
    Added comments outlining which pins we would use. Also established a basic algorithm to vary the duty cycle between 30% and 80% @ 400Hz, it successfully takes the motor from 0RPM to max RPM. (Note, 45% is 0 RPM, 30% is just going extra low).

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 // Radio Frequency Receiver Output: Period is 18.4ms (~54Hz), Duty cycle is 8%, varies between 6% to 10%
 // Motor Control output signal: Period is 2.5 ms (~400Hz), Duty cycle is 44% @ 0 thrust (up to 50% seems to do nothing), 80% duty cycle is 100% thrust (at least from flight controller)
 
+// NOTE: Only FTM channels 0-3 can be used for input capture. Channels 4-7 can be used for PWM output
+// 5 Radio Receiver input Pins: PTD0 (FTM3_Ch0), PTC4 (FTM0_Ch3), PTC3 (FTM0_Ch2), PTC2 (FTM0_Ch1), PTC1 (FTM0_Ch0)
+// 4 Motor Control Output Pins: PTA0 (FTM0_Ch5), PTA2(FTM0_Ch7), PTA1(FTM0_Ch6), PTC12 (FTM3_FLT0)
 
 /*******************************************************************************
  * Definitions
@@ -20,8 +23,14 @@ PwmOut pwmOut1(PTA2);
 
 uint16_t CnV;
 uint16_t CnVPrev;
+float pwmDutyCycle = 0.5f;
 
 void calculatePeriod(void) {
+    pwmDutyCycle += 0.01f;
+    pwmDutyCycle = pwmDutyCycle > .8 ? 0.3 : pwmDutyCycle;
+    pwmOut1.write(pwmDutyCycle);
+
+    printf("Duty Cycle is %f\r\n",pwmDutyCycle);
 	//float frequency = (float)FTM_READ_INPUT_CAPTURE/float(pwmFreqCalculationPeriod)*1000;
 	//float period = 1/frequency;
     CnVPrev = CnV;
@@ -73,11 +82,12 @@ int main() {
     FTM3->MOD = 0xFFFF; // Modulo register must not be 0 for counter to work, see documentation
     FTM_StartTimer(FTM3, kFTM_SystemClock);
 
-    pwmOut1.period(0.020f); // 20ms
-    pwmOut1.write(0.50f);
+    pwmOut1.period(0.0025f); // 2.5ms
+    pwmOut1.write(0.80f);
+    wait(5); // Give the motor time to initialize
 
     // creates a queue to hold 1 events
     EventQueue queue(1*EVENTS_EVENT_SIZE);
-    queue.call_every(1,calculatePeriod); //10ms
+    queue.call_every(300,calculatePeriod); //10ms
     queue.dispatch();
 }
